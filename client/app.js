77b8a72fa5f62f4b6c27481889200e8914ff326b
mdraselmiah95 <russelroy15@gmail.com>
2022-11-25 21:23:29 +0600

    Open Weather App

--- client/app.js
+++ client/app.js
@@ -16,15 +16,15 @@ const masterHistory = document.getElementById("master-history");
 const API_KEY = "00648c3df9cb0d2748fb5dbdaa57cfae";
 const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
 const ICON_URL = "http://openweathermap.org/img/wn/";
-const DEFAULT_CITY = "dhaka,bd";
+const DEFAULT_CITY = "khulna,bd";
 
 //https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=00648c3df9cb0d2748fb5dbdaa57cfae
 //http://openweathermap.org/img/wn/10d@2x.png
 
 window.onload = function () {
   navigator.geolocation.getCurrentPosition(
-    (s) => {
-      getWeatherData(null, s.coords);
+    (success) => {
+      getWeatherData(null, success.coords);
     },
     (error) => {
       getWeatherData(DEFAULT_CITY);
@@ -40,7 +40,18 @@ function getWeatherData(city = DEFAULT_CITY, coords) {
 
   axios
     .get(url)
-    .then((response) => console.log(response.data))
+    .then(({ data }) => {
+      let weather = {
+        icon: data.weather[0].icon,
+        name: data.name,
+        country: data.sys.country,
+        main: data.weather[0].main,
+        description: data.weather[0].description,
+        temp: data.main.temp,
+        pressure: data.main.pressure,
+        humidity: data.main.humidity,
+      };
+    })
     .catch((error) => {
       console.log(error);
     });
