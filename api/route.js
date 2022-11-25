61354adccb15caec31b6e338fb4741a1697f04ab
mdraselmiah95 <russelroy15@gmail.com>
2022-11-26 01:51:26 +0600

    Learning Javascript Oper Weather App backend

--- api/route.js
+++ api/route.js
@@ -1,6 +1,19 @@
 const route = require("express").Router();
 const History = require("./History");
 
+route.get("/", (req, res) => {
+  History.find()
+    .then((history) => {
+      res.status(200).json(history);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error Occurred 💥",
+      });
+    });
+});
+
 route.post("/", (req, res) => {
   let history = new History(req.body);
   history
@@ -12,7 +25,7 @@ route.post("/", (req, res) => {
     .catch((error) => {
       console.log(error);
       res.status(500).json({
-        message: "Error Occurred",
+        message: "Error Occurred 💥",
       });
     });
 });
