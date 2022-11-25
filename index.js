071b8916ccf4a46e8bec0446146a8ed304a64cf3
mdraselmiah95 <russelroy15@gmail.com>
2022-11-26 01:33:58 +0600

    Learning Javascript Oper Weather App backend

--- index.js
+++ index.js
@@ -11,4 +11,11 @@ app.use(express.static("public"));
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log("App is Running on PORT" + " " + PORT);
+  mongoose.connect(
+    `mongodb+srv://weatherApp:<1234567890>@cluster0.4kpgg9t.mongodb.net/?retryWrites=true&w=majority`,
+    { useNewUrlParser: true },
+    () => {
+      console.log("Database Connected");
+    }
+  );
 });
